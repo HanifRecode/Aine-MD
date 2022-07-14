9bf62c0aa1279bab6ee7179b9c89f0a0edad4097
HanifRecode <109059641+HanifRecode@users.noreply.github.com>
2022-07-14 18:20:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6281529467548'],
+  ['6281529467548'],
+  ['6281529467548', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
